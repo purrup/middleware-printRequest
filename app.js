8556ea1151acaf985d3da7591e328f52b8727935
Jerry Tsai <purrup27@gmail.com>
2019-05-18 16:49:18 +0800

    remove home.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const responseTime = require('response-time')
 
 app.use(responseTime())
 
-app.use('/', require('./routes/home'))
-
 // 列出全部 Todo
 app.get('/', printRequest, (req, res) => {
   res.send('列出全部 Todo')
