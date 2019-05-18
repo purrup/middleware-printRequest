4230a0ae99ea2838c8109607f60f95cc2724b034
Jerry Tsai <purrup27@gmail.com>
2019-05-19 00:44:11 +0800

    rebuild printRequest

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const { printRequest } = require('./config/printRequest')
+const moment = require('moment')
 const responseTime = require('response-time')
-const exphbs = require('express-handlebars')
-
+// const exphbs = require('express-handlebars')
 // app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 // app.set('view engine', 'handlebars')
+
+const printRequest = (req, res, next) => {
+  const now = moment().format('HH:mm:ss')
+  const date = new Date()
+  const today = `${date.getFullYear()}-${date.getMonth() +
+    1}-${date.getDate()} ${now}`
+  const method = req.method
+  const url = req.url
+  const timeStamps = `${today} | ${method} from ${url}`
+  console.log(timeStamps)
+  next()
+  const respondTime = res.get('X-Response-Time')
+  const result = `${timeStamps} | total time: ${respondTime}`
+  console.log(result)
+}
+
 app.use(responseTime())
+app.use(printRequest)
 
 // 列出全部 Todo
-app.get('/', printRequest, (req, res) => {
+app.get('/', (req, res) => {
   res.send('列出全部 Todo')
-  // res.render('index')
 })
 
 // 新增一筆 Todo 頁面
-app.get('/new', printRequest, (req, res) => {
+app.get('/new', (req, res) => {
   res.send('新增 Todo 頁面')
 })
 
 // 顯示一筆 Todo 的詳細內容
-app.get('/:id', printRequest, (req, res) => {
+app.get('/:id', (req, res) => {
   res.send('顯示一筆 Todo')
 })
 
 // 新增一筆  Todo
-app.post('/', printRequest, (req, res) => {
+app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
 })
 
-app.delete('/:id/delete', printRequest, (req, res) => {
+app.delete('/:id/delete', (req, res) => {
   res.send('刪除 Todo')
 })
 
